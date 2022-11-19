6adf7157bbf09e7e0e476925323c56b789f55e20
kirubakaran k <87497972+tekdragon0101-kiruba@users.noreply.github.com>
2022-11-19 22:01:44 +0530

    adding Usage manual

--- TED_TALK_Downloader.py
+++ TED_TALK_Downloader.py
@@ -1,3 +1,13 @@
+"""
+This Program is any kind of download video from ted talk website
+Usage:  python3 TED_TALK_Downloader.py https://ted.com/talk/aljkflakjfl.mp4
+        python3 TED_TALK_Downloader.py <URL>
+        
+#### If this program create error when download video content from web, Sorry for that. ####
+"""
+
+
+
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -15,13 +25,12 @@ soup = BeautifulSoup(page.content, 'lxml')
 script_video = soup.find_all('script', id='__NEXT_DATA__')
 # getting the link of video
 video_link = re.search(r'((https?:\/\/)\S+\.mp4)', str(script_video))
-video_title = re.search(
-    r'\"title\":\"([\S\s\,\-]+)\"\,\"socialTitle\"\:', str(script_video))
-print(video_title.group(1), ':', video_link.group(1))
-print('Download started...')
+video_title = re.search(r'\"title\":\"([\S\s\,\-]+)\"\,\"socialTitle\"\:', str(script_video))
+print(video_title.group(1), ':', video_link.group(1)) # printing the link and title of video
+print('Download started...') 
 # getting file download
-video = requests.get(video_link.group(1))
 print('File storing as a ' + video_title.group(1))
+video = requests.get(video_link.group(1))
 try:
     if '\"*:<>?/\\|' not in video_title.group(1):
         with open(video_title.group(1)+'.mp4', 'wb') as f:
